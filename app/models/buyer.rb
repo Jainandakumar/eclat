c0642abd77cbbba6ebf125100ede8949f1e62f7a
Jainandakumar <jainandakumar@yahoo.com>
2021-03-17 18:43:43 +0530

    added dependent destroy

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,7 +1,7 @@
 class Buyer < ApplicationRecord
 
-	has_many :couriers
-	has_many :teams
+	has_many :couriers, dependent: :destroy
+	has_many :teams, dependent: :destroy
 	
 	validates :name, {presence: true, uniqueness: {case_sensitive: true}, length: {minimum: 3, maximum: 75}}
 	validates :address, {presence: true, length: {minimum: 3, maximum: 500}}
