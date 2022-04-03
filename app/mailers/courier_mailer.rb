21aff6b94a4d714ab9ff6b9dcb0d538263707a9f
Jainandakumar <jainandakumar@yahoo.com>
2022-04-03 23:59:33 +0530

    added image in mail

--- app/mailers/courier_mailer.rb
+++ app/mailers/courier_mailer.rb
@@ -6,6 +6,10 @@ class CourierMailer < ApplicationMailer
    	@courier = courier
    	mail_ids = @courier.team.team_members.pluck(:email)
    	@items = @courier.items
+      @items.order(:serial_number).each_with_index do |item, index|
+         blob = item.item_image.blob
+         attachments[index.to_s+blob.filename.to_s] = item.item_image.download
+      end
    	attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/ei_logo.jpg") if @courier.buyer.is_eclat
    	attachments[@courier.airway_bill_number+'.pdf'] = WickedPdf.new.pdf_from_string(
       render_to_string(pdf: 'sample_photos', template: 'items/sample_photos.pdf.erb', locals: {items: @items}))
