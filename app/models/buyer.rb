c50753d121f719359fc6a324b35f905ca4a8dcd5
Jainandakumar <jai.kumar@plansource.com>
2023-06-08 20:48:38 +0530

    added logic for buyer to admin

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -9,6 +9,16 @@ class Buyer < ApplicationRecord
 	validates :name, {presence: true, uniqueness: {case_sensitive: true}, length: {minimum: 3, maximum: 75}}
 	validates :address, {presence: true, length: {minimum: 3, maximum: 500}}
 
+	after_save :add_buyer_to_admin
+
+	def add_buyer_to_admin
+		admin = User.first
+		unless admin.buyer_ids.include?(self.id)
+			admin.buyer_ids.push(self.id)
+			admin.save
+		end
+	end
+
 	def company_upcase
 		company.upcase
 	end
